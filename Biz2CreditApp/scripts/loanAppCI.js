96af2d68794b8fa2b254fc1ba12441ef200ce4f8
Ramit Arora <ravic@biz2credit.com>
2014-09-11 02:35:56 -0500

    Changes made by Ramit Arora (ravic@biz2credit.com).

--- Biz2CreditApp/scripts/loanAppCI.js
+++ Biz2CreditApp/scripts/loanAppCI.js
@@ -881,8 +881,9 @@
                 {
                     if(dataParam['contact_act'] === "Next")
                     {
-                        $msg= "Contact Information submitted successfully";
-                        app.loginService.viewModel.mobileNotification($msg,'info');
+                        //$msg= "Contact Information submitted successfully";
+                        //app.loginService.viewModel.mobileNotification($msg,'info');
+                        
                         sessionStorage.setItem("setprefilStatus",false);
                         app.loanAppCI.viewModel.manageHiddenField(data[0]['results']['onwerids']);
                         apps.navigate('views/loanAppPI.html');
