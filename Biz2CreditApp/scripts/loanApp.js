de8d305f45dd76fadea9f86f738bb95ee1cd1235
Ramit Arora <ravic@biz2credit.com>
2014-09-26 00:40:30 -0500

    Changes made by Ramit Arora (ravic@biz2credit.com).

--- Biz2CreditApp/scripts/loanApp.js
+++ Biz2CreditApp/scripts/loanApp.js
@@ -1,22 +1,59 @@
 (function (global) {
-    var loanApplicationViewModal,
+    var loanAppViewModal,
         app = global.app = global.app || {};
 
-    loanApplicationViewModal = kendo.data.ObservableObject.extend({
+    loanAppViewModal = kendo.data.ObservableObject.extend({
         username: "",
         show:function()
         {
-        $("#tabstrip").kendoTabStrip({
-                    animation:  {
-                        open: {
-                            effects: "fadeIn"
+            $("#tabstrip").kendoTabStrip({
+                        animation:  {
+                            open: {
+                                effects: "fadeIn"
+                            }
                         }
+                    });
+            
+        
+            app.loginService.viewModel.showloder();  
+            var dataSource = new kendo.data.DataSource({
+                transport: {
+                    read: {
+                        url: 'http://sandbox.biz2services.com/mobapp/api/loanapp',
+                        type:"POST",
+                        dataType: "json", // "jsonp" is required for cross-domain requests; use "json" for same-domain requests
+                        data: { apiaction:"manageapp",cust_id:localStorage.getItem("userID")} // search for tweets that contain "html5"
                     }
-                });
-        }
+                    
+                },
+                schema: {
+                    data: function(data)
+                    {
+                    	return [data];
+                    }
+                },
+                error: function (e) {
+                	apps.hideLoading();
+                	navigator.notification.alert("Server not responding properly.Please check your internet connection.",
+                	function () { }, "Notification", 'OK');
+                },
+            });
+            dataSource.fetch(function(){
+                var that = this;
+                var data = that.data();
+                console.log(data);
+                app.loginService.viewModel.hideloder();  
+
+            });
+       },
+        applyFreshLoan:function(e)
+        {
+          app.loginService.viewModel.application(e);  
+        },
+        
     });
    
-    app.loanApplication = {
-        viewModel: new loanApplicationViewModal()	
+    app.loanApp = {
+        viewModel: new loanAppViewModal()	
     };
 })(window);
