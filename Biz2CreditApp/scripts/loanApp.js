c79ae501ef2f25aa62947f67d5c186d0bf476f7a
Ramit Arora <ravic@biz2credit.com>
2014-10-06 07:28:58 -0500

    Changes made by Ramit Arora (ravic@biz2credit.com).

--- Biz2CreditApp/scripts/loanApp.js
+++ Biz2CreditApp/scripts/loanApp.js
@@ -7,15 +7,12 @@
         postApp:0,
         endedApp:0,
         savedApp:0,
-        loanApa:0,
         postAppList:[],
         endedAppList:[],
         savedAppList:[],
-        loanApaList:[],
         postAppTab:true,
         endedAppTab:false,
         savedAppTab:false,
-        loanApaTab:false,
         showrefreshLoan:true,
         
         show:function()
@@ -93,12 +90,6 @@
                     that.set('endedApp',value['appcount']);
                     that.set('endedAppList',value['appdetails']);
                 }
-                if(value['apptype']==='loanApa')
-                {
-                    that.set('loanApa',value['appcount']);
-                    that.set('loanApaList',value['appdetails']);
-                    
-                }
         	});   
           
         },
@@ -110,7 +101,6 @@
             that.set('postAppTab',true);
             that.set('endedAppTab',false);
             that.set('savedAppTab',false);
-            that.set('loanApaTab',false);
         },
         endedAppClick:function(e)
         {
@@ -120,7 +110,6 @@
             that.set('postAppTab',false);
             that.set('endedAppTab',true);
             that.set('savedAppTab',false);
-            that.set('loanApaTab',false);
         },
         savedAppClick:function(e)
         {
@@ -130,17 +119,6 @@
             that.set('postAppTab',false);
             that.set('endedAppTab',false);
             that.set('savedAppTab',true);
-            that.set('loanApaTab',false);
-        },
-        loanApaClick:function(e)
-        {
-            var that=this;
-            $('#tabstrip ul li').removeClass('k-state-active');
-            $('#tabstrip ul li.lon_icon').addClass('k-state-active');
-            that.set('postAppTab',false);
-            that.set('endedAppTab',false);
-            that.set('savedAppTab',false);
-            that.set('loanApaTab',true);
         },
         refreshViewLoan:function()
         {
