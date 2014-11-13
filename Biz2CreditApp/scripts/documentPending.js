3a4431c88f3b7d2ea48f5f908c71da5f1d88cb4e
Ramit Arora <ravic@biz2credit.com>
2014-11-13 10:41:05 +0000

    Changes made by Ramit Arora (ravic@biz2credit.com).

--- Biz2CreditApp/scripts/documentPending.js
+++ Biz2CreditApp/scripts/documentPending.js
@@ -6,13 +6,15 @@
         show:function()
         {
             /*Upload Document Toggle*/
+            $(".documentList .wrap-content h2").unbind('.myPlugin');
+            $(".documentList .wrap-content .row .name a").unbind(".myPlugin");
             
-            $(".documentList .wrap-content h2").on("click", function() {                                    
+            $(".documentList .wrap-content h2").on("click.myPlugin", function() {                                    
                 $(this).next(".rows").slideToggle();
                 $(this).toggleClass("off");
             });
 
-            $(".documentList .wrap-content .row .name a").on("click", function() {
+            $(".documentList .wrap-content .row .name a").on("click.myPlugin", function() {
                 $(this).parents('.row').next(".filesBlock").slideToggle();	
             });
         },
@@ -20,6 +22,10 @@
         {
             apps.navigate("views/document_attach.html");
         },
+        gobackMatchesPage:function()
+        {
+            apps.navigate("views/matches.html");
+        }
     });
     app.documentService ={
         viewModel:new DocumentPendingModel()
